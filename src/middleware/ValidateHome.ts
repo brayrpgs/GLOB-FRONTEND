add3a04540cbb7bb4ff66733f4e01f556ef260ca
BrayRPGs <brayanymelquisedec@gmail.com>
2025-10-29 01:32:49 -0600

    refactor: Remove console.table statements from ValidateHome methods

--- src/middleware/ValidateHome.ts
+++ src/middleware/ValidateHome.ts
@@ -9,13 +9,11 @@ class ValidateHome extends Validate {
   // validate method
   async validateWithLogin (): Promise<void> {
     const user: User = await this.getUser()
-    console.table(user)
     // user.MEMBERSHIPPLAN_ID ?? this.redirect() // uncommented after implements MEMBERSHIPPLAN
   }
 
   async getUser (): Promise<User> {
     const token = new TokenHelper(localStorage.getItem(TOKEN_KEY_NAME) as string)
-    console.table(token)
     const request = new RequestHelper(
       USER_API_SECURITY_URL,
       METHOD_HTTP.GET,
@@ -25,7 +23,6 @@ class ValidateHome extends Validate {
     )
     request.addHeaders('accept', 'application/json')
     const user = await request.buildRequest<User[]>()
-    console.table(user)
     if (user.length > 1 || user.length < 0) this.redirect()
     return user[0]
   }
