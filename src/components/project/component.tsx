8d95f0188340be6fdb685bfdbd682739d95108dd
BrayRPGs <brayanymelquisedec@gmail.com>
2025-10-31 00:40:31 -0600

    feat: Add Project component with animated card display to main page

---
+++ src/components/project/component.tsx
@@ -0,0 +1,46 @@
+import { IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, IonItem, IonLabel, IonList, IonThumbnail } from '@ionic/react'
+import styles from '../../styles/project/styles.module.css'
+import { useEffect } from 'react'
+export const component: React.FC = () => {
+  useEffect(() => { }, [])
+
+  return (
+    <IonCard className={styles.animatedFadeHorizontal}>
+      <IonCardHeader>
+        <IonCardTitle>Card Title</IonCardTitle>
+        <IonCardSubtitle>Card Subtitle</IonCardSubtitle>
+      </IonCardHeader>
+      <IonCardContent>
+        <IonList>
+          <IonItem>
+            <IonThumbnail slot='start'>
+              <img alt='Silhouette of mountains' src='https://ionicframework.com/docs/img/demos/thumbnail.svg' />
+            </IonThumbnail>
+            <IonLabel>Item</IonLabel>
+          </IonItem>
+
+          <IonItem>
+            <IonThumbnail slot='start'>
+              <img alt='Silhouette of mountains' src='https://ionicframework.com/docs/img/demos/thumbnail.svg' />
+            </IonThumbnail>
+            <IonLabel>Item</IonLabel>
+          </IonItem>
+
+          <IonItem>
+            <IonThumbnail slot='start'>
+              <img alt='Silhouette of mountains' src='https://ionicframework.com/docs/img/demos/thumbnail.svg' />
+            </IonThumbnail>
+            <IonLabel>Item</IonLabel>
+          </IonItem>
+
+          <IonItem lines='none'>
+            <IonThumbnail slot='start'>
+              <img alt='Silhouette of mountains' src='https://ionicframework.com/docs/img/demos/thumbnail.svg' />
+            </IonThumbnail>
+            <IonLabel>Item</IonLabel>
+          </IonItem>
+        </IonList>
+      </IonCardContent>
+    </IonCard>
+  )
+}
--- src/pages/main/page.tsx
+++ src/pages/main/page.tsx
@@ -2,6 +2,7 @@ import { IonContent, IonPage } from '@ionic/react'
 import { component as Header } from '../../components/header/component'
 import { component as Footer } from '../../components/footer/component'
 import { ValidateHome } from '../../middleware/ValidateHome'
+import { component as Project } from "../../components/project/component";
 import { useEffect } from 'react'
 
 const Page: React.FC = () => {
@@ -18,9 +19,7 @@ const Page: React.FC = () => {
       <Header isLoggedIn />
       <IonContent className='ion-padding'>
         <h1>Quick Report</h1>
-        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quam, laboriosam nihil unde enim rem reprehenderit voluptatibus ad voluptate minima, cumque, eius veritatis. Delectus autem aspernatur, officiis ab et quis illum.
-          Sunt, quaerat. Minus, tempore cupiditate. Omnis distinctio consequuntur dolore delectus ex cupiditate recusandae earum atque nulla consectetur assumenda vero aliquid veniam quibusdam odio voluptatibus, mollitia, voluptas minus reiciendis impedit ullam!
-        </p>
+        <Project />
       </IonContent>
       <Footer />
     </IonPage>
