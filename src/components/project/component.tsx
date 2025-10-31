8b7dc1c2f199dbc617922e7d50c5ed74c0dcee5d
BrayRPGs <brayanymelquisedec@gmail.com>
2025-10-31 01:24:54 -0600

    feat: Update Project component layout and styling for improved display

--- src/components/project/component.tsx
+++ src/components/project/component.tsx
@@ -1,46 +1,30 @@
-import { IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, IonItem, IonLabel, IonList, IonThumbnail } from '@ionic/react'
+import { IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, IonIcon, IonItem, IonLabel, IonList } from '@ionic/react'
 import styles from '../../styles/project/styles.module.css'
 import { useEffect } from 'react'
+import { layers, listOutline, pricetagOutline } from 'ionicons/icons'
 export const component: React.FC = () => {
   useEffect(() => { }, [])
 
   return (
     <IonCard className={styles.animatedFadeHorizontal}>
       <IonCardHeader>
+        <IonIcon className={styles.iconProject} icon={layers} size='large' />
         <IonCardTitle>Card Title</IonCardTitle>
-        <IonCardSubtitle>Card Subtitle</IonCardSubtitle>
+        <IonCardSubtitle className={styles.suptitleCard}>
+          <span><IonIcon icon={listOutline} color='primary' /> List Issues</span>
+        </IonCardSubtitle>
       </IonCardHeader>
       <IonCardContent>
         <IonList>
-          <IonItem>
-            <IonThumbnail slot='start'>
-              <img alt='Silhouette of mountains' src='https://ionicframework.com/docs/img/demos/thumbnail.svg' />
-            </IonThumbnail>
-            <IonLabel>Item</IonLabel>
-          </IonItem>
-
-          <IonItem>
-            <IonThumbnail slot='start'>
-              <img alt='Silhouette of mountains' src='https://ionicframework.com/docs/img/demos/thumbnail.svg' />
-            </IonThumbnail>
-            <IonLabel>Item</IonLabel>
-          </IonItem>
-
-          <IonItem>
-            <IonThumbnail slot='start'>
-              <img alt='Silhouette of mountains' src='https://ionicframework.com/docs/img/demos/thumbnail.svg' />
-            </IonThumbnail>
-            <IonLabel>Item</IonLabel>
-          </IonItem>
-
-          <IonItem lines='none'>
-            <IonThumbnail slot='start'>
-              <img alt='Silhouette of mountains' src='https://ionicframework.com/docs/img/demos/thumbnail.svg' />
-            </IonThumbnail>
-            <IonLabel>Item</IonLabel>
+          <IonItem className={styles.suptitleCard}>
+            <span><IonIcon icon={pricetagOutline} color='warning' /> <IonLabel>Item</IonLabel></span>
           </IonItem>
         </IonList>
       </IonCardContent>
     </IonCard>
   )
+}
+
+const getProyects = () => {
+
 }
