0e810d1dec7a2e0bd5beaa66fdc4138e81fcb5a8
BrayRPGs <brayanymelquisedec@gmail.com>
2025-11-04 00:33:17 -0600

    Feature: Enhance project page with edit functionality and validation messages

---
+++ src/common/MessageCommon.ts
@@ -0,0 +1,1 @@
+export const MESSAGE_NOT_EMPTY = (data: string): string => (`${data} field name cannot empty`)
--- src/pages/project/page.tsx
+++ src/pages/project/page.tsx
@@ -1,23 +1,70 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 /**
  * imports components
  */
 import { component as Header } from '../../components/header/component'
 import { component as Footer } from '../../components/footer/component'
-import { IonPage } from '@ionic/react'
+import { IonAlert, IonButton, IonContent, IonIcon, IonInput, IonLabel, IonPage, IonTextarea } from '@ionic/react'
+import { ProjectsUtils } from '../../utils/ProjectsUtils'
+import { GetProject } from '../../models/GetProject'
 import { URLHelper } from '../../Helpers/URLHelper'
+import { Project } from '../../models/Project'
+import { MESSAGE_NOT_EMPTY } from '../../common/MessageCommon'
+import { create } from 'ionicons/icons'
 /**
  * import styles
  */
-
 const Page: React.FC = () => {
+  const [project, setProject] = useState<Project>()
+
+  useEffect(() => {
+    const exec = async (): Promise<void> => {
+      // get data prject
+      const projectBackend = await new ProjectsUtils().get<GetProject>({
+        project_id: new URLHelper().getPathId()
+      })
+      // update project
+      setProject(projectBackend.data[0])
+    }
+    void exec()
+  }, [])
   return (
     <>
       <IonPage>
         <Header isLoggedIn />
-        <h1>Project: name_value
-          {}
-        </h1>
+        <IonContent className='ion-padding'>
+          <h1>{project?.NAME.toUpperCase()}</h1>
+
+          <IonButton
+            id='openAlert'
+            color='dark'
+          >
+            <IonIcon icon={create} />
+            Edit
+          </IonButton>
+          <IonAlert
+            trigger='openAlert'
+            mode='ios'
+            header='Config Project'
+            buttons={[
+              {
+                text: 'Delete',
+                handler: (value) => {}
+              },
+              {
+                text: 'Edit',
+                handler: (value) => {}
+              }
+            ]}
+            inputs={[
+              {
+                type: 'text',
+                value: project?.NAME
+              }
+            ]}
+          />
+
+        </IonContent>
         <Footer />
       </IonPage>
     </>
