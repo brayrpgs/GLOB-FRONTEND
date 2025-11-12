86007524ce9b929e86252949d7c8ef028ab5efac
KendallFallas07 <Kendall.Fallas.Mena@gmail.com>
2025-11-12 15:04:50 -0600

    Refactor user productivity logic and update styles; replace ProductivityUtils with GraphsUtils

--- src/utils/GraphsUtils.ts
+++ src/utils/GraphsUtils.ts
@@ -1,5 +1,5 @@
-import { IssueUtils } from '../utils/IssueUtils'
-import { UserProjectUtils } from '../utils/UserProjectUtils'
+import { IssueUtils } from './IssueUtils'
+import { UserProjectUtils } from './UserProjectUtils'
 import { URLHelper } from '../Helpers/URLHelper'
 import { GetIssues } from '../models/GetIssues'
 
@@ -8,23 +8,22 @@ export async function getUserProductivity (userId: number): Promise<{ total: num
     const projectId = new URLHelper().getPathId()
     const issueUtils = new IssueUtils()
     const userProjectUtils = new UserProjectUtils()
-    const userProjects: any = await userProjectUtils.get<any>({ user_id_fk: userId })
 
-    const projectList = Array.isArray(userProjects)
-      ? userProjects
-      : userProjects?.data ?? []
+    const userProjectsResponse = await userProjectUtils.get<any>({ user_id_fk: userId })
+    const userProjects = Array.isArray(userProjectsResponse)
+      ? userProjectsResponse
+      : userProjectsResponse?.data ?? []
 
-    console.log(projectList)
-
-    const isInProject: boolean = Boolean(projectList?.some((p: any) => p?.USER_PROJECT_ID === projectId))
-    if (!isInProject) {
+    if (userProjects.length === 0) {
       return { total: 0, done: 0 }
     }
 
     const issuesResponse = await issueUtils.get<GetIssues>({ project_id_fk: projectId })
     const issues = issuesResponse.Issues ?? []
 
-    const userIssues = issues.filter((i) => i.USER_ASSIGNED_FK === userId)
+    const userProjectIds = userProjects.map((up: any) => up.USER_PROJECT_ID)
+    const userIssues = issues.filter((i) => userProjectIds.includes(i.USER_ASSIGNED_FK))
+
     const total = userIssues.length
     const done = userIssues.filter((i) => i.STATUS_ISSUE === 3).length
 
