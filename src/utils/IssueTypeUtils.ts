0c59964f8e614b410e0f93eddeb66285ade4da07
BrayRPGs <brayanymelquisedec@gmail.com>
2025-11-11 23:59:35 -0600

    Feature/issues edit (#53)
    * Enhance issue interaction: add cursor style and hover effect, implement conditional visibility for buttons
    
    * Enhance issue management: add sprint selection and issue type retrieval, improve form handling with validation
    
    * Enhance issue handling: add validation for required fields and improve error handling in issue update process

--- src/utils/IssueTypeUtils.ts
+++ src/utils/IssueTypeUtils.ts
@@ -6,7 +6,19 @@ import { TokenPayload } from '../models/TokenPayload'
 
 class IssueTypeUtils implements Api {
   token (): string { return localStorage.getItem(TOKEN_KEY_NAME) as string }
-  get!: <T>(tokenPayload?: TokenPayload, body?: any, params?: any, path?: any) => Promise<T>;
+  async get<T>(params?: any): Promise<T> {
+    const request = new RequestHelper(
+      ISSUES_TYPE_API_DATA_APLICATION_URL,
+      METHOD_HTTP.GET,
+      RESPONSE_TYPE.JSON,
+      null,
+      params
+    )
+    request.addHeaders('accept', 'application/json')
+    request.addHeaders('Authorization', this.token())
+    return await request.buildRequest()
+  }
+
   async post<T>(body: any): Promise<T> {
     const request = new RequestHelper(
       ISSUES_TYPE_API_DATA_APLICATION_URL,
@@ -21,7 +33,19 @@ class IssueTypeUtils implements Api {
   }
 
   put!: <T>(tokenPayload?: TokenPayload, body?: any, params?: any, path?: any) => Promise<T>
-  patch!: <T>(tokenPayload?: TokenPayload, body?: any, params?: any, path?: any) => Promise<T>
+  async patch<T>(body: any, path: any): Promise<T> {
+    const request = new RequestHelper(
+      `${ISSUES_TYPE_API_DATA_APLICATION_URL}${path as number}`,
+      METHOD_HTTP.PATCH,
+      RESPONSE_TYPE.JSON,
+      body
+    )
+    request.addHeaders('accept', 'application/json')
+    request.addHeaders('Content-Type', 'application/json')
+    request.addHeaders('Authorization', this.token())
+    return await request.buildRequest()
+  }
+
   delete!: <T>(tokenPayload?: TokenPayload, body?: any, params?: any, path?: any) => Promise<T>
   head!: <T>(tokenPayload?: TokenPayload, body?: any, params?: any, path?: any) => Promise<T>
 }
